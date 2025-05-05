0ded67d7c29b2793cdd0eaff412014b69ee5f0ac
rafaeldbo <rafaeldbo@al.insper.edu.br>
2025-05-04 20:55:53 -0400

    alterando nome do módulo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'recon=app.main:main',
+            'recon=recon.main:main',
         ],
     },
 )
